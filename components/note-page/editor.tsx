b84b6a4874aa471a6e123854c64dfa76b547a606
AJ Straight <ajstraight@AJs-MacBook-Pro.local>
2024-02-16 12:46:08 -0800

    testing new blot

--- components/note-page/editor.tsx
+++ components/note-page/editor.tsx
@@ -5,9 +5,31 @@ import "katex/dist/katex.min.css";
 import { useEffect, useRef, useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import CustomLatexBlot from "./custom-blot";
-Quill.register("formats/customLatex", CustomLatexBlot, true);
 window.katex = katex;
+
+const BlockEmbed = Quill.import("blots/block/embed");
+
+class LatexBlot extends BlockEmbed {
+  static create(value: string) {
+    let node = super.create();
+    // Render LaTeX using KaTeX
+    katex.render(value, node, {
+      throwOnError: false,
+      errorColor: "#ff0000",
+    });
+    return node;
+  }
+
+  static value(domNode: HTMLElement) {
+    return domNode.innerText;
+  }
+}
+
+LatexBlot.blotName = "latex";
+LatexBlot.tagName = "div";
+LatexBlot.className = "ql-latex";
+
+Quill.register("formats/latex", LatexBlot);
 
 const quillModules = {
   toolbar: [

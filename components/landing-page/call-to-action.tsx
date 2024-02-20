e547efba1b53f7a04ce51e372cd2c3b0d8ef34e7
Alex Straight <compsciaj@outlook.com>
2024-02-19 22:48:01 -0800

    added continue to dashboard button on landing page if user has a session

--- components/landing-page/call-to-action.tsx
+++ components/landing-page/call-to-action.tsx
@@ -1,19 +1,40 @@
 "use client";
 
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
+import {
+  LoginLink,
+  RegisterLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
 
 const CallToAction = () => {
+  const { user } = useKindeBrowserClient();
   return (
     <div
       id="home"
       className="flex flex-row items-center justify-center gap-4 mt-20"
     >
-      <RegisterLink className="border-2 bg-primary dark:text-black text-lg font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-primary/50 hover:shadow-gray-500/50 transition duration-300 ease-in-out">
-        Sign up
-      </RegisterLink>
-      <LoginLink className="border-2  bg-transparent dark:text-white text-lg font-semibold py-3 px-6 rounded-lg  hover:bg-zinc-900 hover:shadow-gray-500/50 shadow-md  transition duration-300 ease-in-out">
-        Log in
-      </LoginLink>
+      {user ? (
+        <Link
+          className="group border-2 flex px-3 py-6 rounded-lg text-black transition duration-300 ease-in-out bg-gradient-to-r from-gray-200 to-gray-400"
+          href="/dashboard"
+        >
+          Continue to dashboard
+          <span className="ml-2 transition-transform transform translate-x-0 group-hover:translate-x-1">
+            <MoveRight />
+          </span>
+        </Link>
+      ) : (
+        <>
+          <RegisterLink className="border-2 bg-primary py-3 px-6 rounded-lg text-black">
+            Sign up
+          </RegisterLink>
+          <LoginLink className="border-2  bg-transparent py-3 px-6 rounded-lg">
+            Log in
+          </LoginLink>
+        </>
+      )}
     </div>
   );
 };

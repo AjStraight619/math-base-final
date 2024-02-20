77bd78056886c3d3188abb310a68eb78161307b1
Alex Straight <compsciaj@outlook.com>
2024-02-19 22:31:43 -0800

    added speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveItemContextProvider from "@/context/active-item-context";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import SidebarContextProvider from "@/context/sidebar-context";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
@@ -47,6 +48,7 @@ export default async function RootLayout({
             <Toaster />
           </ThemeProvider>
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </>
